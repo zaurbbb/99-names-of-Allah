9fcf7b9b4890a5a00c3a6778f57341dd40ce7db2
zaurbbb <zaurberdibekov@mail.ru>
2023-01-15 00:38:18 +0600

    finish layout of ShowResults

--- src/components/quiz/ShowResults/ShowResults.jsx
+++ src/components/quiz/ShowResults/ShowResults.jsx
@@ -2,20 +2,34 @@ import React, { useContext } from 'react';
 
 import { QuizContext } from '../../../context/quiz';
 
+import css from './ShowResults.module.sass';
+import { FormattedMessage } from 'react-intl';
+import CustomButton from '../../ui/custom/CustomButton/CustomButton';
+import { Link } from 'react-router-dom';
+
 const ShowResults = () => {
     const [quizState, dispatch] = useContext(QuizContext);
+
+    function handleRestartTest() {
+        dispatch({ type: 'RESTART' });
+    }
+
     return (
-        <div className='results'>
-            <div className='congratulations'>
-                Congratulations
-            </div>
-            <div className='results-info'>
-                <div>You've completed the quiz <br/><br/></div>
-                <div>You've got {quizState.correctAnswersCount} of {quizState.questions.length} <br/><br/></div>
-                <div
-                    className='next-button'
-                    onClick={() => dispatch({type: 'RESTART'})}
-                >Restart</div>
+        <div className={css.Block}>
+            <h1>
+                <FormattedMessage id='w.test_results_right_answers1' />
+                {quizState.correctAnswersCount}
+                <FormattedMessage id='w.test_results_right_answers2' />
+            </h1>
+            <div>
+                <CustomButton
+                    filled={true}
+                    text='test_button_repeat'
+                    onClick={handleRestartTest}
+                />
+                <Link to='/'>
+                    <CustomButton text='unavailable_subtext' />
+                </Link>
             </div>
         </div>
     );
